1357e1fc0066e846882f668913ec90ed57133809
PoisonedPigeons <70314018+PoisonedPigeons@users.noreply.github.com>
2024-03-30 21:23:18 -0400

    straight.el related changes and updates
    :ensure keyword replacement and bootstrap update

---
+++ early-init.el
@@ -0,0 +1,3 @@
+;; Needed by the straight.el package manager.
+(when (>= emacs-major-version 27)
+  (setq package-enable-at-startup nil))
--- init.el
+++ init.el
@@ -1,7 +1,4 @@
-;    -*- eval: (flycheck-mode 0) -*-
-(when (< emacs-major-version 27)
-  (package-initialize))
 (require 'org)
 (org-babel-load-file
  (expand-file-name "ReadMe.org"
-                   user-emacs-directory))
+		   user-emacs-directory))
